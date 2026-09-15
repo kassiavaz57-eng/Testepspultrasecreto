eefb48a8e4f27205986f0df912fe1891c19e25a4
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-15 12:45:55 -0300

    Fix missing closing brace in PSP renderer diagnostics

--- psp/psp_renderer.c
+++ psp/psp_renderer.c
@@ -221,5 +221,6 @@ static void pspDrawRectangle(Renderer *renderer,float x1,float y1,float x2,float
     (void)renderer; uint32_t c=bgrToGu(color,alpha); PSPVertex *v=(PSPVertex*)sceGuGetMemory(4*sizeof(PSPVertex));
     float tx1=(x1-g_viewX)*g_scaleX+g_offX, ty1=(y1-g_viewY)*g_scaleY+g_offY;
     float tx2=(x2-g_viewX)*g_scaleX+g_offX, ty2=(y2-g_viewY)*g_scaleY+g_offY;
-    v[0]=(PSPVertex){0,0,c,tx1,ty1,0};v[1]=(PSPVertex){0,0,c,tx2,ty1,0};v[2]=(PSPVertex){0,0,c,tx2,ty2,0};v[3]=(PSPVertex){0,0,c,tx1,ty2,0};
-    sceGuDisable(GU_TEXTURE_2D);sceGuDrawArray(outline?GU_LINE_STRIP:GU_TRIANGLE_FAN,GU_COLOR_8888|GU_VERTEX_32BITF|GU_TRANSFORM_2D,4,NULL,v);sceGuEnable(GU_TEXTURE_2D);
+    v[0]=(PSPVertex){0,0,c,tx1,ty1,0};v[1]=(PSPVertex){0,0,c,tx2,ty1,0};v[2]=(PSPVertex){0,0,c,tx2,ty2,0};v[3]=(PSPVertex){0,0,c,tx1,ty2,0}
+
+static void pspDiagDummyLink(void){ pspDiagReport(); }
