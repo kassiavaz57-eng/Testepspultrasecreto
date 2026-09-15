ec58197302868f6ff9444365cdb0613e9ec30fa0
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-15 00:52:24 -0300

    Enable TXTR parsing for PSP renderer

--- psp/psp_main.c
+++ psp/psp_main.c
@@ -21,7 +21,7 @@ static int exit_cb(int a,int b,void*c){(void)a;(void)b;(void)c;sceKernelExitGame
 static int cb_thread(SceSize a,void*b){(void)a;(void)b;int cb=sceKernelCreateCallback("Exit Callback",exit_cb,NULL);if(cb>=0)sceKernelRegisterExitCallback(cb);sceKernelSleepThreadCB();return 0;}
 static void setup_callbacks(void){int t=sceKernelCreateThread("update_thread",cb_thread,0x11,0xFA0,0,NULL);if(t>=0)sceKernelStartThread(t,0,NULL);}
 void platformLog(const logType type,const char*fmt,va_list va){if(type==LOG_TYPE_WARNING)fputs("Warning: ",stdout);else if(type==LOG_TYPE_ERROR)fputs("Error: ",stdout);else if(type==LOG_TYPE_DEBUG)fputs("Debug: ",stdout);vprintf(fmt,va);}
-static bool load_data_win(const char*path,DataWin**out){DataWinParserOptions o={0};o.parseGen8=true;o.parseOptn=true;o.parseLang=true;o.parseExtn=true;o.parseSond=true;o.parseAgrp=true;o.parseSprt=true;o.parseBgnd=true;o.parsePath=true;o.parseScpt=true;o.parseGlob=true;o.parseShdr=true;o.parseFont=true;o.parseTmln=true;o.parseObjt=true;o.parseRoom=true;o.parseTpag=true;o.parseCode=true;o.parseVari=true;o.parseFunc=true;o.parseStrg=true;o.parseTxtr=false;o.parseAudo=false;o.skipLoadingPreciseMasksForNonPreciseSprites=true;o.lazyLoadRooms=true;o.lazyLoadTextures=true;o.lazyLoadAudio=true;o.loadType=DATAWINLOADTYPE_LOAD_PER_CHUNK;*out=DataWin_parse(path,o);return *out!=NULL;}
+static bool load_data_win(const char*path,DataWin**out){DataWinParserOptions o={0};o.parseGen8=true;o.parseOptn=true;o.parseLang=true;o.parseExtn=true;o.parseSond=true;o.parseAgrp=true;o.parseSprt=true;o.parseBgnd=true;o.parsePath=true;o.parseScpt=true;o.parseGlob=true;o.parseShdr=true;o.parseFont=true;o.parseTmln=true;o.parseObjt=true;o.parseRoom=true;o.parseTpag=true;o.parseCode=true;o.parseVari=true;o.parseFunc=true;o.parseStrg=true;o.parseTxtr=true;o.parseAudo=false;o.skipLoadingPreciseMasksForNonPreciseSprites=true;o.lazyLoadRooms=true;o.lazyLoadTextures=true;o.lazyLoadAudio=true;o.loadType=DATAWINLOADTYPE_LOAD_PER_CHUNK;*out=DataWin_parse(path,o);return *out!=NULL;}
 int main(void){setup_callbacks();logInfo("Butterscotch PSP: boot\n");const char*root="ms0:/PSP/GAME/BUTTERSCOTCH";const char*path="ms0:/PSP/GAME/BUTTERSCOTCH/data.win";DataWin*d=NULL;if(!load_data_win(path,&d)){logError("Could not load %s\n",path);sceKernelSleepThread();return 1;}logInfo("WAD %u, %ux%u\n",d->gen8.wadVersion,d->gen8.defaultWindowWidth,d->gen8.defaultWindowHeight);VMContext*vm=VM_create(d);Renderer*r=PSPRenderer_create();AudioSystem*a=(AudioSystem*)NoopAudioSystem_create();FileSystem*fs=PspFileSystem_create(root);if(!vm||!r||!a||!fs){logError("Subsystem initialization failed\n");sceKernelSleepThread();return 1;}Runner*runner=Runner_create(d,vm,r,fs,a,0);if(!runner){logError("Runner_create failed\n");sceKernelSleepThread();return 1;}runner->osType=OS_PSP;Runner_initFirstRoom(runner);
 sceDisplayWaitVblankStart();
 for(;;){
@@ -38,7 +38,3 @@ for(;;){
     Runner_drawPost(runner,480,272);
     runner->renderer->vtable->endFrameEnd(runner->renderer);
     Runner_drawGUI(runner,480,272,gameW,gameH);
-    RunnerKeyboard_beginFrame(runner->keyboard);
-    sceGuFinish(); sceGuSync(GU_SYNC_FINISH,GU_SYNC_WHAT_DONE);
-    sceDisplayWaitVblankStart(); sceGuSwapBuffers();
-}sceKernelExitGame();return 0;}
