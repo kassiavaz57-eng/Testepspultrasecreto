89ce9d6e336b84fa96ec4f1a813722a0d1d33815
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-15 21:59:32 -0300

    PSP: allow enough cached glyph textures for full text strings

--- psp/psp_renderer.c
+++ psp/psp_renderer.c
@@ -24,7 +24,7 @@
 #define PSP_TEX_MAX 512
 typedef struct { float u,v; unsigned int color; float x,y,z; } PSPVertex;
 static unsigned int __attribute__((aligned(16))) g_list[65536/sizeof(unsigned int)];
-#define PSP_TEX_CACHE_ENTRIES 16
+#define PSP_TEX_CACHE_ENTRIES 128
 #define PSP_TEX_CACHE_BYTES (8u*1024u*1024u)
 typedef struct {
     int valid;
