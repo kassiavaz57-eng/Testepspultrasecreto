b9aaf522c0335fb0334cc0415e4530129e8adef6
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-16 19:31:00 -0300

    Use PSn00bSDK standard ordering-table GPU calibration

--- ps1/main.c
+++ ps1/main.c
@@ -4,47 +4,93 @@
 
 #define W 320
 #define H 240
+#define OT_LEN 8
+#define PACKET_LEN 1024
 
-static DISPENV disp;
-static DRAWENV draw;
-static TILE tile;
+typedef struct {
+    DISPENV disp;
+    DRAWENV draw;
+    uint32_t ot[OT_LEN];
+    uint8_t packet[PACKET_LEN];
+} Buffer;
 
-static void rect(int x,int y,int w,int h,int r,int g,int b){
-    setTile(&tile);
-    setXY0(&tile,x,y);
-    setWH(&tile,w,h);
-    setRGB0(&tile,r,g,b);
-    DrawPrim(&tile);
-}
+static Buffer buf;
 
-static void video_init(void){
+static void init_video(void) {
     ResetGraph(0);
-    SetDefDispEnv(&disp,0,0,W,H);
-    SetDefDrawEnv(&draw,0,H,W,H);
-    draw.isbg=1;
-    setRGB0(&draw,0,0,0);
-    PutDispEnv(&disp);
-    PutDrawEnv(&draw);
+
+    SetDefDispEnv(&buf.disp, 0, 0, W, H);
+    SetDefDrawEnv(&buf.draw, 0, H, W, H);
+
+    buf.draw.isbg = 1;
+    buf.draw.dtd = 1;
+    setRGB0(&buf.draw, 0, 0, 0);
+
+    ClearOTagR(buf.ot, OT_LEN);
+    PutDispEnv(&buf.disp);
+    PutDrawEnv(&buf.draw);
     SetDispMask(1);
 }
 
-static void pattern(void){
-    rect(0,0,W,H,0,0,0);
-    rect(4,4,28,28,0,0,255);
-    rect(W-32,4,28,28,0,255,0);
-    rect(4,H-32,28,28,255,0,0);
-    rect(W-32,H-32,28,28,255,255,0);
-    rect(156,116,8,8,255,255,255);
-    rect(0,119,W,2,255,255,255);
-    rect(159,0,2,H,255,255,255);
+static void draw_calibration(void) {
+    TILE *p;
+    uint8_t *next = buf.packet;
+
+    ClearOTagR(buf.ot, OT_LEN);
+
+    p = (TILE *)next;
+    next += sizeof(TILE);
+    setTile(p);
+    setXY0(p, 4, 4);
+    setWH(p, 28, 28);
+    setRGB0(p, 0, 0, 255);
+    addPrim(&buf.ot[OT_LEN - 1], p);
+
+    p = (TILE *)next;
+    next += sizeof(TILE);
+    setTile(p);
+    setXY0(p, W - 32, 4);
+    setWH(p, 28, 28);
+    setRGB0(p, 0, 255, 0);
+    addPrim(&buf.ot[OT_LEN - 1], p);
+
+    p = (TILE *)next;
+    next += sizeof(TILE);
+    setTile(p);
+    setXY0(p, 4, H - 32);
+    setWH(p, 28, 28);
+    setRGB0(p, 255, 0, 0);
+    addPrim(&buf.ot[OT_LEN - 1], p);
+
+    p = (TILE *)next;
+    next += sizeof(TILE);
+    setTile(p);
+    setXY0(p, W - 32, H - 32);
+    setWH(p, 28, 28);
+    setRGB0(p, 255, 255, 0);
+    addPrim(&buf.ot[OT_LEN - 1], p);
+
+    p = (TILE *)next;
+    next += sizeof(TILE);
+    setTile(p);
+    setXY0(p, 156, 116);
+    setWH(p, 8, 8);
+    setRGB0(p, 255, 255, 255);
+    addPrim(&buf.ot[OT_LEN - 1], p);
+
     DrawSync(0);
+    VSync(0);
+    PutDispEnv(&buf.disp);
+    PutDrawEnv(&buf.draw);
+    DrawOTag(&buf.ot[OT_LEN - 1]);
 }
 
-int main(void){
-    video_init();
-    for(;;){
-        pattern();
-        VSync(0);
+int main(void) {
+    init_video();
+
+    for (;;) {
+        draw_calibration();
     }
+
     return 0;
 }
