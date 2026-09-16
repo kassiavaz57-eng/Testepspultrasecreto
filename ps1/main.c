52effc5850d2ea76394e2818400a529f651896e8
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-16 19:38:05 -0300

    Match official PSn00bSDK double-buffer render sequence

--- ps1/main.c
+++ ps1/main.c
@@ -1,11 +1,10 @@
 #include <stdint.h>
-#include <psxetc.h>
 #include <psxgpu.h>
 
-#define SCREEN_W 320
-#define SCREEN_H 240
+#define W 320
+#define H 240
 #define OT_LEN 16
-#define PACKET_LEN 2048
+#define PACKET_LEN 4096
 
 typedef struct {
     DISPENV disp;
@@ -14,81 +13,98 @@ typedef struct {
     uint8_t packet[PACKET_LEN];
 } RenderBuffer;
 
-static RenderBuffer buffers[2];
-static uint8_t *next_packet;
-static int db;
+typedef struct {
+    RenderBuffer b[2];
+    uint8_t *next;
+    int active;
+} RenderContext;
+
+static RenderContext ctx;
 
 static void init_video(void) {
     ResetGraph(0);
-    SetVideoMode(MODE_NTSC);
 
-    SetDefDispEnv(&buffers[0].disp, 0, 0, SCREEN_W, SCREEN_H);
-    SetDefDrawEnv(&buffers[0].draw, 0, SCREEN_H, SCREEN_W, SCREEN_H);
+    SetDefDrawEnv(&ctx.b[0].draw, 0, 0, W, H);
+    SetDefDispEnv(&ctx.b[0].disp, 0, 0, W, H);
+    SetDefDrawEnv(&ctx.b[1].draw, 0, H, W, H);
+    SetDefDispEnv(&ctx.b[1].disp, 0, H, W, H);
 
-    SetDefDispEnv(&buffers[1].disp, 0, SCREEN_H, SCREEN_W, SCREEN_H);
-    SetDefDrawEnv(&buffers[1].draw, 0, 0, SCREEN_W, SCREEN_H);
+    setRGB0(&ctx.b[0].draw, 0, 0, 0);
+    setRGB0(&ctx.b[1].draw, 0, 0, 0);
+    ctx.b[0].draw.isbg = 1;
+    ctx.b[1].draw.isbg = 1;
 
-    setRGB0(&buffers[0].draw, 0, 0, 0);
-    setRGB0(&buffers[1].draw, 0, 0, 0);
-    buffers[0].draw.isbg = 1;
-    buffers[1].draw.isbg = 1;
-    buffers[0].draw.dtd = 1;
-    buffers[1].draw.dtd = 1;
+    ctx.active = 0;
+    ctx.next = ctx.b[0].packet;
+    ClearOTagR(ctx.b[0].ot, OT_LEN);
 
-    db = 0;
-    next_packet = buffers[0].packet;
-    ClearOTagR(buffers[0].ot, OT_LEN);
-    ClearOTagR(buffers[1].ot, OT_LEN);
-
-    PutDispEnv(&buffers[0].disp);
-    PutDrawEnv(&buffers[0].draw);
     SetDispMask(1);
 }
 
-static void add_tile(int x, int y, int w, int h, int r, int g, int b) {
-    RenderBuffer *buf = &buffers[db];
-    TILE *tile = (TILE *)next_packet;
-
-    setTile(tile);
-    setXY0(tile, x, y);
-    setWH(tile, w, h);
-    setRGB0(tile, r, g, b);
-
-    addPrim(&buf->ot[1], tile);
-    next_packet += sizeof(TILE);
+static void *alloc_prim(int z, int size) {
+    RenderBuffer *rb = &ctx.b[ctx.active];
+    uint8_t *p = ctx.next;
+    addPrim(&rb->ot[z], p);
+    ctx.next += size;
+    return p;
 }
 
-static void frame(void) {
-    RenderBuffer *drawbuf = &buffers[db];
+static void render_frame(void) {
+    RenderBuffer *draw = &ctx.b[ctx.active];
+    RenderBuffer *display = &ctx.b[ctx.active ^ 1];
 
-    next_packet = drawbuf->packet;
-    ClearOTagR(drawbuf->ot, OT_LEN);
+    TILE *t = (TILE *)alloc_prim(1, sizeof(TILE));
+    setTile(t);
+    setXY0(t, 0, 0);
+    setWH(t, W, H);
+    setRGB0(t, 12, 12, 12);
 
-    add_tile(0, 0, SCREEN_W, SCREEN_H, 8, 8, 8);
-    add_tile(4, 4, 28, 28, 0, 0, 255);
-    add_tile(SCREEN_W - 32, 4, 28, 28, 0, 255, 0);
-    add_tile(4, SCREEN_H - 32, 28, 28, 255, 0, 0);
-    add_tile(SCREEN_W - 32, SCREEN_H - 32, 28, 28, 255, 255, 0);
-    add_tile(156, 116, 8, 8, 255, 255, 255);
-    add_tile(0, 119, SCREEN_W, 2, 255, 255, 255);
-    add_tile(159, 0, 2, SCREEN_H, 255, 255, 255);
+    t = (TILE *)alloc_prim(2, sizeof(TILE));
+    setTile(t);
+    setXY0(t, 4, 4);
+    setWH(t, 28, 28);
+    setRGB0(t, 0, 0, 255);
+
+    t = (TILE *)alloc_prim(2, sizeof(TILE));
+    setTile(t);
+    setXY0(t, W - 32, 4);
+    setWH(t, 28, 28);
+    setRGB0(t, 0, 255, 0);
+
+    t = (TILE *)alloc_prim(2, sizeof(TILE));
+    setTile(t);
+    setXY0(t, 4, H - 32);
+    setWH(t, 28, 28);
+    setRGB0(t, 255, 0, 0);
+
+    t = (TILE *)alloc_prim(2, sizeof(TILE));
+    setTile(t);
+    setXY0(t, W - 32, H - 32);
+    setWH(t, 28, 28);
+    setRGB0(t, 255, 255, 0);
+
+    t = (TILE *)alloc_prim(2, sizeof(TILE));
+    setTile(t);
+    setXY0(t, 156, 116);
+    setWH(t, 8, 8);
+    setRGB0(t, 255, 255, 255);
 
     DrawSync(0);
     VSync(0);
 
-    PutDispEnv(&drawbuf->disp);
-    PutDrawEnv(&drawbuf->draw);
-    SetDispMask(1);
-    DrawOTag(&drawbuf->ot[OT_LEN - 1]);
+    PutDispEnv(&display->disp);
+    DrawOTagEnv(&draw->ot[OT_LEN - 1], &draw->draw);
 
-    db ^= 1;
+    ctx.active ^= 1;
+    ctx.next = display->packet;
+    ClearOTagR(display->ot, OT_LEN);
 }
 
 int main(void) {
     init_video();
 
     for (;;) {
-        frame();
+        render_frame();
     }
 
     return 0;
