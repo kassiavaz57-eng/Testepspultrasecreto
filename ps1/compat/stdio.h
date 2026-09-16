da952523c8aefb68da740335583cb0f0c3cb45b1
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-16 20:29:07 -0300

    Avoid PSn00bSDK seek enum macro collision

--- ps1/compat/stdio.h
+++ ps1/compat/stdio.h
@@ -4,15 +4,6 @@
 #include <stdint.h>
 #include <stdarg.h>
 
-#ifndef SEEK_SET
-#define SEEK_SET 0
-#endif
-#ifndef SEEK_CUR
-#define SEEK_CUR 1
-#endif
-#ifndef SEEK_END
-#define SEEK_END 2
-#endif
 #include <psxcd.h>
 typedef struct {
     CdlFILE cd;
