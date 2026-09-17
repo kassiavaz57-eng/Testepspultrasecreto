30b1c2f1ad410b52b302192052eb1e9ba70c01fd
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-17 20:18:06 -0300

    ps1: initialize heap before runtime allocations

--- ps1/ps1_utils.c
+++ ps1/ps1_utils.c
@@ -3,8 +3,19 @@
 #include "string_compat.h"
 #include "../utils.h"
 #include <psxcd.h>
+#include <psxapi.h>
+
+/* Keep the heap below the top-of-RAM stack with a safety margin. */
+#define PS1_STACK_TOP     0x801FFFF0u
+#define PS1_STACK_RESERVE (16u * 1024u)
+
+extern uint8_t _end[];
 
 void PS1Utils_init(void) {
+    uint32_t heapStart = (uint32_t)_end;
+    uint32_t heapSize = PS1_STACK_TOP - PS1_STACK_RESERVE - heapStart;
+
+    InitHeap((void*)heapStart, heapSize);
     CdInit();
 }
 
