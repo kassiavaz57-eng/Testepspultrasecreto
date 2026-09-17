030bab4e59aa7e0d0e90430a7cd4fa567b0ab825
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-16 22:51:46 -0300

    PS1: expose controller initialization

--- ps1/ps1_gamepad.h
+++ ps1/ps1_gamepad.h
@@ -3,7 +3,7 @@
 
 #include "runner_gamepad.h"
 
-/* PS1 pad backend. PSn00bSDK exposes the controller as PADTYPE. */
+void Ps1Gamepad_init(void);
 void Ps1Gamepad_poll(RunnerGamepadState* gp, int port);
 
 #endif /* _BS_PS1_GAMEPAD_H_ */
