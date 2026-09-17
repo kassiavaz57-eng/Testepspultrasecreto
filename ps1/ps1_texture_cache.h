99bb7fc1993362d43fe827e0aa47394c217c2686
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-16 23:01:34 -0300

    PS1: add real ATLAS/TEXTURES VRAM cache interface

---
+++ ps1/ps1_texture_cache.h
@@ -0,0 +1,76 @@
+#ifndef _BS_PS1_TEXTURE_CACHE_H_
+#define _BS_PS1_TEXTURE_CACHE_H_
+
+#include "common.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <psxgpu.h>
+
+#define PS1_TEXTURE_MAX_SLOTS 16
+#define PS1_CLUT4_CACHE_SLOTS 32
+#define PS1_CLUT8_CACHE_SLOTS 4
+
+typedef struct {
+    uint16_t atlasId;
+    uint16_t atlasX;
+    uint16_t atlasY;
+    uint16_t width;
+    uint16_t height;
+    uint16_t cropX;
+    uint16_t cropY;
+    uint16_t cropW;
+    uint16_t cropH;
+    uint16_t clutIndex;
+} Ps1AtlasTPAGEntry;
+
+typedef struct {
+    uint16_t width;
+    uint16_t height;
+    uint8_t bpp;
+    uint32_t dataSize;
+    uint32_t dataOffset;
+    uint8_t compression;
+} Ps1AtlasInfo;
+
+typedef struct {
+    bool valid;
+    uint16_t atlasId;
+    int16_t x;
+    int16_t y;
+    uint8_t bpp;
+    uint32_t lastUsed;
+} Ps1TextureSlot;
+
+typedef struct {
+    bool valid;
+    uint16_t clutIndex;
+    uint16_t x;
+    uint16_t y;
+    uint32_t lastUsed;
+} Ps1ClutSlot;
+
+typedef struct {
+    Ps1AtlasTPAGEntry* tpag;
+    uint16_t tpagCount;
+    Ps1AtlasInfo* atlases;
+    uint16_t atlasCount;
+    FILE* texturesFile;
+    Ps1TextureSlot textures[PS1_TEXTURE_MAX_SLOTS];
+    Ps1ClutSlot clut4[PS1_CLUT4_CACHE_SLOTS];
+    Ps1ClutSlot clut8[PS1_CLUT8_CACHE_SLOTS];
+    uint32_t frameCounter;
+} Ps1TextureCache;
+
+bool Ps1TextureCache_init(Ps1TextureCache* cache);
+void Ps1TextureCache_shutdown(Ps1TextureCache* cache);
+
+/* Ensures an atlas is resident and returns its PS1 texture page. */
+uint16_t Ps1TextureCache_getTPage(Ps1TextureCache* cache, uint16_t atlasId);
+
+/* Returns the CLUT location encoded for getClut(), or 0 on failure. */
+uint16_t Ps1TextureCache_getClut(Ps1TextureCache* cache, uint8_t bpp, uint16_t clutIndex);
+
+/* Resolves a TPAG entry into the atlas page + UV origin used by POLY_FT4/SPRT. */
+bool Ps1TextureCache_resolveTPAG(Ps1TextureCache* cache, int32_t tpagIndex, uint16_t* outTPage, uint16_t* outClut, int16_t* outU, int16_t* outV, uint16_t* outW, uint16_t* outH);
+
+#endif /* _BS_PS1_TEXTURE_CACHE_H_ */
