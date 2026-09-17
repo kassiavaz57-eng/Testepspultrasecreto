8fe366386b865b12516ffb6f0abd3701640b3093
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-17 19:33:09 -0300

    ps1: declare real strtod ABI for VM runtime

--- ps1/compat/stdio.h
+++ ps1/compat/stdio.h
@@ -35,4 +35,5 @@ int vfprintf(FILE *f, const char *fmt, va_list ap);
 int fputs(const char *s, FILE *f);
 int snprintf(char *str, size_t size, const char *fmt, ...);
 int vsnprintf(char *str, size_t size, const char *fmt, va_list ap);
+double strtod(const char *nptr, char **endptr);
 #endif
