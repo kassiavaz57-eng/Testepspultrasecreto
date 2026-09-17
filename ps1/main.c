b64f9753bac0ab41532a1a6744270b849a6dfbfd
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-16 21:34:22 -0300

    Remove intrusive boot diagnostics and restore clean PS1 entrypoint

--- ps1/main.c
+++ ps1/main.c
@@ -1,36 +1,12 @@
 #include <psxetc.h>
 #include <psxcd.h>
-#include <psxgpu.h>
-#include <psxgte.h>
 #include "loop.h"
 
 #define DATA_WIN_PATH "cdrom:\\DATA.WIN;1"
 
-static void diag_screen(const char *stage) {
-    DISPENV disp;
-    DRAWENV draw;
-    ResetGraph(0);
-    SetDefDispEnv(&disp, 0, 0, 320, 240);
-    SetDefDrawEnv(&draw, 0, 240, 320, 240);
-    draw.isbg = 1;
-    draw.dfe = 0;
-    SetDispMask(1);
-    PutDispEnv(&disp);
-    PutDrawEnv(&draw);
-    FntLoad(960, 0);
-    int id = FntOpen(16, 16, 288, 208, 0, 1024);
-    FntPrint(id, "BUTTERSCOTCH PS1 DEBUG\n\n");
-    FntPrint(id, "MAIN       OK\n");
-    FntPrint(id, "GRAPH      OK\n");
-    FntPrint(id, "CD         OK\n");
-    FntPrint(id, "LOOP       %s\n", stage);
-    FntFlush(-1);
-}
-
 int main(void) {
-    diag_screen("START");
+    ResetGraph(0);
     CdInit();
-    diag_screen("ENTERING");
 
     CommandLineArgs args = {0};
     args.exitAtFrame = -1;
@@ -45,6 +21,5 @@ int main(void) {
     args.renderer = NOOP;
     args.dataWinPath = DATA_WIN_PATH;
 
-    diag_screen("BUTTERSCOTCH LOOP");
     return loop(args, "BUTTER.EXE");
 }
