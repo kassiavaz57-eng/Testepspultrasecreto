405e58492ba577a77686c161beee0c32dc6a09ae
kassiavaz57-eng <kassiavaz57@gmail.com>
2026-09-16 21:21:50 -0300

    Show PS1 boot diagnostics on screen

--- ps1/main.c
+++ ps1/main.c
@@ -1,16 +1,36 @@
 #include <psxetc.h>
 #include <psxcd.h>
+#include <psxgpu.h>
+#include <psxgte.h>
 #include "loop.h"
-#include <stdio.h>
 
 #define DATA_WIN_PATH "cdrom:\\DATA.WIN;1"
 
-int main(void) {
-    printf("PS1: main start\\n");
+static void diag_screen(const char *stage) {
+    DISPENV disp;
+    DRAWENV draw;
     ResetGraph(0);
-    printf("PS1: graph ok\\n");
+    SetDefDispEnv(&disp, 0, 0, 320, 240);
+    SetDefDrawEnv(&draw, 0, 240, 320, 240);
+    draw.isbg = 1;
+    draw.dfe = 0;
+    SetDispMask(1);
+    PutDispEnv(&disp);
+    PutDrawEnv(&draw);
+    FntLoad(960, 0);
+    int id = FntOpen(16, 16, 288, 208, 0, 1024);
+    FntPrint(id, "BUTTERSCOTCH PS1 DEBUG\n\n");
+    FntPrint(id, "MAIN       OK\n");
+    FntPrint(id, "GRAPH      OK\n");
+    FntPrint(id, "CD         OK\n");
+    FntPrint(id, "LOOP       %s\n", stage);
+    FntFlush(-1);
+}
+
+int main(void) {
+    diag_screen("START");
     CdInit();
-    printf("PS1: cd ok\\n");
+    diag_screen("ENTERING");
 
     CommandLineArgs args = {0};
     args.exitAtFrame = -1;
@@ -25,6 +45,6 @@ int main(void) {
     args.renderer = NOOP;
     args.dataWinPath = DATA_WIN_PATH;
 
-    printf("PS1: entering Butterscotch loop\\n");
+    diag_screen("BUTTERSCOTCH LOOP");
     return loop(args, "BUTTER.EXE");
 }
